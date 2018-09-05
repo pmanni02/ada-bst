5b8146f1535ef7f061a32e22794eaa13e200c3df
Phoebe <phoebe.manning8@gmail.com>
2018-09-04 21:12:15 -0700

    partially working in order traversal

--- src/bst.js
+++ src/bst.js
@@ -6,16 +6,65 @@ class BST {
   }
 
   insert(val){
+    if(this.root == null){
+      this.root = new Node(val);
+    } else {
+      this.insertRecursive(this.root, new Node(val));
+    }
 
+    return this.root;
   }
 
-  search(val){
-
+  insertRecursive(currNode, newNode){
+    if(newNode.value <= currNode.value){
+      if(currNode.left === null){
+        currNode.left = newNode;
+        return;
+      } else {
+        this.insertRecursive(currNode.left, newNode);
+      }
+    } else {
+      if(currNode.right === null){
+        currNode.right = newNode;
+        return;
+      } else {
+        this.insertRecursive(currNode.right, newNode);
+      }
+    }
   }
+
+  // search(val){
+  //
+  // }
 
   inOrderPrint(){
-    
+    let printedVals = "";
+    if(this.root == null){
+      return printedVals;
+    }
+    return this.inOrderPrintRecursive(this.root, printedVals);
   }
+
+  inOrderPrintRecursive(root, result){
+    if(root === null){
+      return result;
+    }
+
+    if(root.left != null){
+      result = this.inOrderPrintRecursive(root.left);
+    }
+
+    result += `${root.value} `;
+
+    if(root.right != null){
+      result = this.inOrderPrintRecursive(root.right);
+    }
+    return result;
+
+
+  }
+
+
 }
 
 module.exports = BST;
